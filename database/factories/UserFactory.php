6dc95c90ca893f5e88326e9916fa7d3588550c7f
miku3920 <papa964204@gamil.com>
2023-10-21 15:17:14 +0800

    add method phpcs rule

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Str;
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory {
+
     /**
      * Define the model's default state.
      *
